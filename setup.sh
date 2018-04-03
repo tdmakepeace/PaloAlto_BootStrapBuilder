c422a298652857314e4aa4db04c530f6e455ea26
Toby Makepeace <tdmakepeace@gmail.com>
2018-04-03 19:05:08 +0100

    v0.1

---
+++ setup.sh
@@ -0,0 +1,17 @@
+mkdir Backups
+mkdir ISOImages
+mkdir Master
+mkdir Master/config
+mkdir Master/content
+mkdir Master/license
+mkdir Master/software
+mkdir Build
+mkdir Build/config
+mkdir Build/content
+mkdir Build/license
+mkdir Build/software
+chmod 744 XMLBuilder/env.txt
+chmod 744 XMLBuilder/VMserver.txt
+
+
+
